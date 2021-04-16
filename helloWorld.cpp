1d1cd3bff4a87488138a8f979431b0f7672d247c
Katelin Lewellen <katelin.lewellen@gmail.com>
2021-04-16 09:38:29 -0700

    adding in a header comment for good style

--- helloWorld.cpp
+++ helloWorld.cpp
@@ -1,4 +1,10 @@
- #include <stdio.h>
+// FILE: helloWorld.cpp
+// AUTHOR: katelin lewellen
+// DATE: 2021
+//
+// A simple hello world first program
+
+#include <stdio.h>
 
 int main(int argc, char** argv) {
   printf("Hello World!\n");
